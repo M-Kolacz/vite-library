d3db74f92054acfa762486f3aa53d4f12c3ac1b8
Michał Kołacz <michal.kolacz45@gmail.com>
2024-02-10 11:38:58 +0100

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
+
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      include: ['src/components'],
+    }),
+  ],
+  build: {
+    copyPublicDir: false,
+    cssCodeSplit: true,
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.ts'),
+      name: 'VeryFancyComponents',
+      fileName: 'very-fancy-components',
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        // preserveModules: true,
+      },
+    },
+  },
+});
